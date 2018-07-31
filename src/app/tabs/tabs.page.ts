c2c9df2b7c076597277b76f6961f986975aeada0
Evan Moore <evanm@themoores.ca>
2018-07-31 11:01:44 -0400

    initial commit

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { MenuController, Tab } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {}
+export class TabsPage {
+  constructor(private menuController: MenuController) {
+
+  }
+
+  protected tabChanged(evt: CustomEvent) {
+    const tab: Tab = (evt.detail.tab as Tab);
+    // Make sure we are responding to a tab change
+    if (tab === undefined) {
+      return;
+    }
+    console.log(`tab ${tab.label} activated`);
+    this.menuController.enable(true, tab.label).then(menu => {
+      console.log('enabled menu', menu);
+    });
+  }
+}

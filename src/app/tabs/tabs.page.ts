20355b2dde50e5ec7a0ffaf98eddb50f17e1885c
Evan Moore <evanm@themoores.ca>
2018-12-03 21:49:12 -0500

    update to beta.17

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -12,14 +12,15 @@ export class TabsPage {
   }
 
   protected tabChanged(evt: CustomEvent) {
+    console.log(evt);
     const tab: Tab = (evt.detail.tab as Tab);
     // Make sure we are responding to a tab change
     if (tab === undefined) {
       return;
     }
-    console.log(`tab ${tab.label} activated`);
-    this.menuController.enable(true, tab.label).then(menu => {
+    console.log(`tab ${tab.tab} activated`);
+    this.menuController.enable(true, tab.tab).then(menu => {
+      // this.menuController.open(tab.tab); While this will open the menu, it's not what you expect in a splitview
       console.log('enabled menu', menu);
-    });
-  }
+    });  }
 }
